ab75027afaac0ce531fabb73b44be66e815c6f1a
Yuneido Chacin <146007178+yjchf@users.noreply.github.com>
2024-08-26 17:36:11 +0000

    JS Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,55 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = [
+    "My dog",
+    "The neighbor",
+    "My little brother",
+    "A band of monkeys",
+    "My cat"
+  ];
+  let action = [
+    "ate",
+    "broke",
+    "stole",
+    "burned",
+    "destroyed",
+    "stained",
+    "blow up"
+  ];
+  let what = [
+    "my homework",
+    "my car",
+    "my computer",
+    "my keys",
+    "the report",
+    "the papers"
+  ];
+  let when = [
+    "last night",
+    "this morning",
+    "a while ago",
+    "last week",
+    "while I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuseBody = [who, action, what, when];
+
+  function getValue(arr) {
+    let option = Math.floor(Math.random() * arr.length);
+    return arr[option];
+  }
+
+  function excuseMaker(arrOfArr) {
+    // Actually this function will work with any desired output if you provide an ordered structure of words (like the body I made)
+    let excuseArray = arrOfArr.map(getValue);
+    return excuseArray.join(" ");
+  }
+
+  document.getElementById("excuse").innerHTML = excuseMaker(excuseBody);
+  console.log(document.getElementById("excuse").innerHTML);
 };
